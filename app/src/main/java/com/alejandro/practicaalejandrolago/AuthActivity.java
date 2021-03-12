76422e54a572e9b0b86b6de073b45846f01e350d
Alejandro Lago Pereira <Alejandro.lago@hotmail.com>
2021-03-12 12:26:43 +0100

    ADD: Importamos una dependencia para Firebase Authentication ADD: Añado codigo en "AuthActivity" preparando para recibir y enviar informacion a firebase

--- app/src/main/java/com/alejandro/practicaalejandrolago/AuthActivity.java
+++ app/src/main/java/com/alejandro/practicaalejandrolago/AuthActivity.java
@@ -3,12 +3,63 @@
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class AuthActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+ public class AuthActivity extends AppCompatActivity {
+
+    private EditText txtNombre;
+    private EditText txtEmail;
+    private EditText txtContraseña;
+    private Button btnRegistrarse;
+
+    // Variables de los datos que vamos a registrar
+     private String nombre = "";
+     private String email = "";
+     private String contraseña = "";
+
+     FirebaseAuth Auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        Auth = FirebaseAuth.getInstance();
+
+        txtNombre = (EditText) findViewById(R.id.txtNombre);
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+        txtContraseña = (EditText) findViewById(R.id.txtContraseña);
+        btnRegistrarse = (Button) findViewById(R.id.btnRegistrarse);
+
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nombre = txtNombre.getText().toString();
+                email = txtEmail.getText().toString();
+                contraseña = txtContraseña.getText().toString();
+
+                if(!nombre.isEmpty() && !email.isEmpty() && contraseña.isEmpty()) {
+
+                    if (contraseña.length() >= 6) {
+                        registerUser();
+                    } else {
+                        Toast.makeText(AuthActivity.this, "La contraseña debe tener al menos 6 carácteres", Toast.LENGTH_SHORT);
+                    }
+
+                } else {
+                    Toast.makeText(AuthActivity.this, "Debes completar los campos", Toast.LENGTH_SHORT);
+                }
+            }
+        });
     }
+
+    private void registerUser() {
+
+    }
+
 }

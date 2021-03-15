b4e1d6609d24bb7b3de0aa3c7c627ba986fc6646
Alejandro Lago Pereira <Alejandro.lago@hotmail.com>
2021-03-15 13:05:21 +0100

    UPDATE: Añadido traducciones ADD: Codigo, imagenes de la activity "ImagenActivity" ADD: Añadido arrays.xml en values

--- app/src/main/java/com/alejandro/practicaalejandrolago/ImagenActivity.java
+++ app/src/main/java/com/alejandro/practicaalejandrolago/ImagenActivity.java
@@ -3,6 +3,9 @@ package com.alejandro.practicaalejandrolago;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class ImagenActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class ImagenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagen);
+    }
+
+    //cambioImagen -- Cambia la imagen de la portada
+    //Se ejecutará al presionar el boton de cambiar imagen
+    public void onClickcambiarImagen(View view){
+        //Creo un spinner y lo relaciono al Spinner de pantalla
+        Spinner imagen = (Spinner) findViewById(R.id.spinnerImagenes);
+
+        //Creo un objeto imageView y lo relaciono al Spinner en la pantalla
+        ImageView imagePort = (ImageView) findViewById(R.id.imagenPortada);
+
+        //Leo lo que está seleccionado en el spinner
+        String img = String.valueOf(imagen.getSelectedItem());
+
+        //Cambio la imagen de la portada
+        if(img.equals("arknights")){
+            imagePort.setImageResource(R.drawable.arknights);
+        }else if (img.equals("lol")){
+            imagePort.setImageResource(R.drawable.lol);
+        }else if (img.equals("closers")){
+            imagePort.setImageResource(R.drawable.closers);
+        }else{
+            imagePort.setImageResource(R.drawable.genshin);
+        }
     }
 }
